9413e33394b9f77fafbda0ad41445d190de1e15c
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-13 19:53:50 +0530

    commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) => {
+  res.send("login users");
+};
+
+export const logout = (req, res) => {
+  res.send("login users");
+};
+
+export const signup = (req, res) => {
+  res.send("login users");
+};
