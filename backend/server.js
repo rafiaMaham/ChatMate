66da34042b7bd52839fc4ca9ea19e86d87a0f1b1
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-16 20:26:04 +0530

    get users route and functionality except the logged in users

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectMongo from "./db/connectMongo.js";
 
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectMongo();
