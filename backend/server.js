631cc7652581637860f1749a0422da5308d9f1eb
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-16 18:36:32 +0530

    message controller, protectRoute

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectMongo from "./db/connectMongo.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 dotenv.config();
-app.use(express.json())
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectMongo();
