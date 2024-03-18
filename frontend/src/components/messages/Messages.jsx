eeed696f682d513cbe2676182832d1f51a855d9d
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-18 23:20:19 +0530

    message container UI

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,18 @@
+import Message from "./Message.jsx";
+
+const Messages = () => {
+  return (
+    <div className="flex-1 px-4 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
